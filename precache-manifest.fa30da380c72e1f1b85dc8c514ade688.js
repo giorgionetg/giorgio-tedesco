fa66ace29a0ddbefa2ed5e7e1a66a487e371090d
Giorgio Tedesco <gt@giorgiotedesco.it>
2019-03-13 14:37:33 +0100

    react app
    test

---
+++ precache-manifest.fa30da380c72e1f1b85dc8c514ade688.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/giorgio-tedesco/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92a0b7694b845c6f61cd",
+    "url": "/giorgio-tedesco/static/js/runtime~main.037cb908.js"
+  },
+  {
+    "revision": "44c1b4f4bc3334d388fd",
+    "url": "/giorgio-tedesco/static/js/main.a27489cd.chunk.js"
+  },
+  {
+    "revision": "2d884e061f16a67987bb",
+    "url": "/giorgio-tedesco/static/js/2.76c17010.chunk.js"
+  },
+  {
+    "revision": "44c1b4f4bc3334d388fd",
+    "url": "/giorgio-tedesco/static/css/main.41c5dfb2.chunk.css"
+  },
+  {
+    "revision": "2d884e061f16a67987bb",
+    "url": "/giorgio-tedesco/static/css/2.4e9f11a2.chunk.css"
+  },
+  {
+    "revision": "6979dc26bebaa752feeb814d49a8ae2b",
+    "url": "/giorgio-tedesco/index.html"
+  }
+];
