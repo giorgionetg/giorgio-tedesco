79ca4ecd9f9661b62c1c2dd7c5b4be86df82e46b
Giorgio Tedesco <gt@giorgiotedesco.it>
2019-03-15 11:29:05 +0100

    Add files via upload

---
+++ precache-manifest.1d8677b8d9d17c8abd1d1753869ef286.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/giorgio-tedesco/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92a0b7694b845c6f61cd",
+    "url": "/giorgio-tedesco/static/js/runtime~main.037cb908.js"
+  },
+  {
+    "revision": "0d6880a1ebc3a9422fd4",
+    "url": "/giorgio-tedesco/static/js/main.0d28bf06.chunk.js"
+  },
+  {
+    "revision": "89ad756e296a6793a9df",
+    "url": "/giorgio-tedesco/static/js/2.e8dbdd5b.chunk.js"
+  },
+  {
+    "revision": "0d6880a1ebc3a9422fd4",
+    "url": "/giorgio-tedesco/static/css/main.354b4002.chunk.css"
+  },
+  {
+    "revision": "89ad756e296a6793a9df",
+    "url": "/giorgio-tedesco/static/css/2.24389b64.chunk.css"
+  },
+  {
+    "revision": "f31d895950f4155615f8ef7dacbcefc3",
+    "url": "/giorgio-tedesco/index.html"
+  }
+];
